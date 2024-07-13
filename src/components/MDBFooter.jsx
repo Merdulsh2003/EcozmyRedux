3d87488348448f0842e1f8e95dbfe1e9b2126c04
Merdul Sharma <merdulsharma2003@gmail.com>
2024-07-13 18:35:09 +0530

    MDBFooter.jsx

--- src/components/MDBFooter.jsx
+++ src/components/MDBFooter.jsx
@@ -9,7 +9,7 @@ export default function MDBFooter() {
    <>
       <div className="w-[100%] flex flex-row text-md bg-gray-200 mt-16 text-center gap-4 items-center justify-center">
               <div className="text-center items-center">
-                Created By Vagish Maurya &copy; 2023
+                2024 Merdul Sharma. All rights reserved.
               </div>
               <div className="text-center items-center">
                 <HighLightText text={"ECOZMY"}/>
